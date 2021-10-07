02fb8bd8c7bb7bde69ee07e3eed1e95c83f71a01
Fernando Nogueira <fernandonogueira@ntadvogados.com.br>
2021-10-07 00:20:41 -0300

    Initial commit

---
+++ src/main/java/faj/poo/usina/UsinaApplication.java
@@ -0,0 +1,13 @@
+package faj.poo.usina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faj/poo/usina/UsinaApplicationTests.java
@@ -0,0 +1,13 @@
+package faj.poo.usina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
